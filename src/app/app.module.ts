8a3d1cbec508aeece61e9dd58dd721827c09cc98
Nathan <l.nathan97460@gmail.com>
2022-03-02 15:14:31 +0100

    New component : main
     Changes to be committed:
    	modified:   angular.json
    	modified:   src/app/app.component.html
    	modified:   src/app/app.module.ts
    	new file:   src/app/main/main.component.css
    	new file:   src/app/main/main.component.html
    	new file:   src/app/main/main.component.ts
    	new file:   src/flexboxgrid.min.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
